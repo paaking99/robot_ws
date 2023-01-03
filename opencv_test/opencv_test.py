02e842a0c93182a2710058f30b704f3b82d91bc0
paaking99 <timtkddn@gmail.com>
2023-01-03 00:37:27 -0800

    s

--- opencv_test/opencv_test.py
+++ opencv_test/opencv_test.py
@@ -2,8 +2,17 @@ import cv2
 import numpy as np
 
 print(cv2.__version__)
-image = cv2.imread('./image/face.jpeg',cv2.IMREAD_UNCHANGED)
-print(image.shape)
-# cv2.imshow('imagewindow', image)
-# cv2.waitKey(0)
-# cv2.destroyWindow()
+image = cv2.imread('./image/face2.jpeg',cv2.IMREAD_UNCHANGED)
+height, width, channel = image.shape()
+print(height)
+print(width)
+print(channel)
+#변환
+image_flip = cv2.flip(image,0)
+image_flip = cv2.flip(image,1)
+#회전
+image_rotate = cv2.getRotationMatrix2D(width/2, height/2,30,1)
+cv2.imshow('imagewindow1',image)
+cv2.imshow('imagewindow2', image_rotate)
+cv2.waitKey(0)
+cv2.destroyWindow()
